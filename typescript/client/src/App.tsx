f2f19ae47428f119291569ac2bb84e3248339a63
Julian Perassi <perassijulian@gmail.com>
2022-03-12 17:23:02 -0300

    start front end

--- typescript/client/src/App.tsx
+++ typescript/client/src/App.tsx
@@ -1,25 +1,21 @@
-import { useState } from 'react';
 import './styles.scss';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 
-import AddTask from './components/AddTask';
-import Task from './components/Task';
-
-import { Todo } from './interfaces';
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([])
 
   return (
     <div className="App">
-      <h1>Tasks</h1>
-      <AddTask 
-        todos={todos} 
-        setTodos={setTodos} 
-      />
-      <Task 
-        todos={todos} 
-        setTodos={setTodos}
-      />
+      <Router>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/books" element={<h1>Books</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
